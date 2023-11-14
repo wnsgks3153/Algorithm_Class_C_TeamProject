3bee8a87c1dafad6e6a96ceee54f3bad90bd012b
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2023-11-14 17:52:16 +0900

    20231114_1.1.5_update
    항목별로 검색하는 기능 구현

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
     long long int category;   // 도서분류
@@ -41,12 +42,33 @@ void quickSort(BookData arr[], int left, int right) {
 }
 
 // 각 자릿수를 분리하여 구조체 멤버에 저장
-void parseCode(long long int code, int* category, int* codeNum, int* year, int* month, int* day) {
-    *category = (int)(code / 100000000000);          // 10억으로 나누면 천의 자리가 남음
-    *codeNum = (int)((code / 100000000) % 100000);     // 백만으로 나눈 나머지를 천으로 나누면 백의 자리가 남음
-    *year = (int)((code % 100000000) / 10000);       // 10000으로 나누면 연도가 나옴
-    *month = (int)((code % 10000) / 100);          // 100으로 나누면 월이 나옴
-    *day = (int)(code % 100);                      // 100으로 나눈 나머지가 일
+void parseCode(long long int code, long long int* category, long long int* codeNum, long long int* year, long long int* month, long long int* day) {
+    *category = code / 100000000000;          // 10억으로 나누면 천의 자리가 남음
+    *codeNum = (code / 100000000) % 100000;   // 백만으로 나눈 나머지를 천으로 나누면 백의 자리가 남음
+    *year = (code % 100000000) / 10000;       // 10000으로 나누면 연도가 나옴
+    *month = (code % 10000) / 100;            // 100으로 나누면 월이 나옴
+    *day = code % 100;                        // 100으로 나눈 나머지가 일
+}
+
+// 검색 함수
+void searchBooks(BookData arr[], int dataSize, long long int category, long long int code, long long int year, long long int month, long long int day) {
+    bool found = false;
+
+    // 검색된 도서코드를 출력
+    for (int i = 0; i < dataSize; i++) {
+        if ((category == -1 || arr[i].category == category) &&
+            (code == -1 || arr[i].code == code) &&
+            (year == -1 || arr[i].year == year) &&
+            (month == -1 || arr[i].month == month) &&
+            (day == -1 || arr[i].day == day)) {
+            printf("도서분류: %03d%03d%04d%02d%02d\n", arr[i].category, arr[i].code, arr[i].year, arr[i].month, arr[i].day);
+            found = true;
+        }
+    }
+
+    if (!found) {
+        printf("일치하는 도서코드가 없습니다.\n");
+    }
 }
 
 int main() {
@@ -95,10 +117,27 @@ int main() {
     // 퀵 정렬 수행
     quickSort(data, 0, dataSize - 1);
 
-    // 정렬된 데이터 출력
-    for (int i = 0; i < dataSize; i++) {
-        printf("도서분류: %03d, 분류기호: %03d, 년: %04d, 월: %02d, 일: %02d\n", data[i].category, data[i].code, data[i].year, data[i].month, data[i].day);
-    }
+    // 검색 기능
+    long long int searchCategory = -1;  // -1은 해당 항목을 무시하고 검색
+    long long int searchCode = -1;
+    long long int searchYear = -1;
+    long long int searchMonth = -1;
+    long long int searchDay = -1;
+
+    // 사용자 입력을 받아 검색 조건 설정
+    printf("도서분류를 입력하세요 (-1은 무시): ");
+    scanf("%lld", &searchCategory);
+    printf("분류기호를 입력하세요 (-1은 무시): ");
+    scanf("%lld", &searchCode);
+    printf("년을 입력하세요 (-1은 무시): ");
+    scanf("%lld", &searchYear);
+    printf("월을 입력하세요 (-1은 무시): ");
+    scanf("%lld", &searchMonth);
+    printf("일을 입력하세요 (-1은 무시): ");
+    scanf("%lld", &searchDay);
+
+    // 검색 함수 호출
+    searchBooks(data, dataSize, searchCategory, searchCode, searchYear, searchMonth, searchDay);
 
     // 할당된 메모리 해제
     free(data);
