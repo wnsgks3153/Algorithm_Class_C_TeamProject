01bfbac2cdc8c923a68000fbd2678999f2ca7cc4
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2023-11-14 13:31:53 +0900

    파싱하여 분류해서 줄력하는 기능 업데이트

--- test.c
+++ test.c
@@ -1,21 +1,30 @@
-#define _CRT_SECURE_NO_WARNINGS()
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+typedef struct {
+    long long int category;   // 도서분류
+    long long int code;       // 분류기호
+    long long int year;       // 연도
+    long long int month;      // 월
+    long long int day;        // 일
+} BookData;
 
 // 퀵 정렬 함수
-void quickSort(long long int arr[], int left, int right) {
+void quickSort(BookData arr[], int left, int right) {
     int i = left, j = right;
-    long long int pivot = arr[(left + right) / 2];
+    BookData pivot = arr[(left + right) / 2];
 
     // 분할
     while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
+        while (arr[i].category < pivot.category || (arr[i].category == pivot.category && arr[i].code < pivot.code) || (arr[i].category == pivot.category && arr[i].code == pivot.code && (arr[i].year < pivot.year || (arr[i].year == pivot.year && (arr[i].month < pivot.month || (arr[i].month == pivot.month && arr[i].day < pivot.day))))))
+            i++;
+        while (arr[j].category > pivot.category || (arr[j].category == pivot.category && arr[j].code > pivot.code) || (arr[j].category == pivot.category && arr[j].code == pivot.code && (arr[j].year > pivot.year || (arr[j].year == pivot.year && (arr[j].month > pivot.month || (arr[j].month == pivot.month && arr[j].day > pivot.day))))))
+            j--;
         if (i <= j) {
             // Swap
-            long long int temp = arr[i];
+            BookData temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
@@ -26,6 +35,15 @@ void quickSort(long long int arr[], int left, int right) {
     // 재귀적으로 정렬
     if (left < j) quickSort(arr, left, j);
     if (i < right) quickSort(arr, i, right);
+}
+
+// 각 자릿수를 분리하여 구조체 멤버에 저장
+void parseCode(long long int code, int* category, int* codeNum, int* year, int* month, int* day) {
+    *category = (int)(code / 100000000000);          // 10억으로 나누면 천의 자리가 남음
+    *codeNum = (int)((code / 100000000) % 100000);     // 백만으로 나눈 나머지를 천으로 나누면 백의 자리가 남음
+    *year = (int)((code % 100000000) / 10000);       // 10000으로 나누면 연도가 나옴
+    *month = (int)((code % 10000) / 100);          // 100으로 나누면 월이 나옴
+    *day = (int)(code % 100);                      // 100으로 나눈 나머지가 일
 }
 
 int main() {
@@ -40,7 +58,7 @@ int main() {
     int initialSize = 30000;
 
     // 데이터 배열 동적 할당
-    long long int* data = malloc(initialSize * sizeof(long long int));
+    BookData* data = malloc(initialSize * sizeof(BookData));
     if (data == NULL) {
         perror("메모리를 할당할 수 없습니다.");
         fclose(file);
@@ -52,13 +70,14 @@ int main() {
     int currentSize = initialSize;
 
     // 파일에서 데이터 읽기
-    while (fscanf(file, "%lld", &data[dataSize]) == 1) {
+    while (fscanf(file, "%lld", &data[dataSize].code) == 1) {
+        parseCode(data[dataSize].code, &data[dataSize].category, &data[dataSize].code, &data[dataSize].year, &data[dataSize].month, &data[dataSize].day);
         dataSize++;
 
         // 배열 크기가 부족하면 두 배로 확장
         if (dataSize == currentSize) {
             currentSize *= 2;
-            data = realloc(data, currentSize * sizeof(long long int));
+            data = realloc(data, currentSize * sizeof(BookData));
             if (data == NULL) {
                 perror("메모리를 재할당할 수 없습니다.");
                 fclose(file);
@@ -73,10 +92,9 @@ int main() {
     // 퀵 정렬 수행
     quickSort(data, 0, dataSize - 1);
 
-    // 정렬된 데이터 출력 또는 저장 등을 수행할 수 있음
-    // 여기에서는 간단히 출력만 수행
+    // 정렬된 데이터 출력
     for (int i = 0; i < dataSize; i++) {
-        printf("%lld\n", data[i]);
+        printf("도서분류: %03d, 분류기호: %03d, 날짜: %04d%02d%02d\n", data[i].category, data[i].code, data[i].year, data[i].month, data[i].day);
     }
 
     // 할당된 메모리 해제
