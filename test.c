f6a7f1ade6b1e72d32802f12bf5b5dbe9f7bfe4e
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2023-11-16 00:41:42 +0900

    20231116_1.1.11_update
    프로그램 시작시 검색,추가,수정,삭제,사용종료를 선택하게 메인함수를 변경, 각 항목의 새로운 함수를 추가

--- test.c
+++ test.c
@@ -58,39 +58,31 @@ void parseCode(long long int code, long long int* category, long long int* codeN
     *day = code % 100;                        // 100으로 나눈 나머지가 일
 }
 
+
 // 검색 결과를 형식에 맞게 출력
 void printSearchResult(int resultIndex, BookData book) {
     printf("%d. %03lld%03lld%04lld%02lld%02lld\n", resultIndex, book.category, book.code, book.year, book.month, book.day);
 }
 
+// 중복된 숫자를 처리하고 고유한 숫자로 만드는 함수
+void removeDuplicateDigits(char* input) {
+    int len = strlen(input);
+    int currentIndex = 1;
 
-// 검색 함수
-void searchBooks(BookData arr[], int dataSize, long long int category, long long int code, long long int year, long long int month, long long int day) {
-    bool found = false;
-    int resultIndex = 0;
-
-    CLEAR_SCREEN();
-
-    printf("[ 검색 결과 ]\n");
-
-    // 검색된 도서코드를 출력
-    for (int i = 0; i < dataSize; i++) {
-        if (
-            (category == arr[i].category || category < 0) &&
-            (code == arr[i].code || code < 0) &&
-            (year == arr[i].year || year < 0) &&
-            (month == arr[i].month || month < 0) &&
-            (day == arr[i].day || day < 0)
-            ) {
-            resultIndex++;
-            printSearchResult(resultIndex, arr[i]);
-            found = true;
+    for (int i = 1; i < len; i++) {
+        int j;
+        for (j = 0; j < currentIndex; j++) {
+            if (input[i] == input[j]) {
+                break;
+            }
+        }
+        if (j == currentIndex) {
+            input[currentIndex] = input[i];
+            currentIndex++;
         }
     }
 
-    if (!found) {
-        printf("일치하는 도서코드가 없습니다.\n");
-    }
+    input[currentIndex] = '\0'; // 문자열 끝에 널 문자 추가
 }
 
 // 검색 입력을 받는 함수
@@ -119,7 +111,6 @@ int isDigitPresent(char* input, char digit) {
     return 0; // 숫자가 존재하지 않음
 }
 
-// 각 숫자에 따라 사용자에게 검색 기준을 입력 받는 함수
 // 각 숫자에 따라 사용자에게 검색 기준을 입력 받는 함수
 void promptUserForSearchCriteria(char* input, long long int* searchCategory, long long int* searchCode, long long int* searchYear, long long int* searchMonth, long long int* searchDay) {
     if (isDigitPresent(input, '1')) {
@@ -159,25 +150,177 @@ void promptUserForSearchCriteria(char* input, long long int* searchCategory, lon
     }
 }
 
-// 중복된 숫자를 처리하고 고유한 숫자로 만드는 함수
-void removeDuplicateDigits(char* input) {
-    int len = strlen(input);
-    int currentIndex = 1;
+// 검색 함수
+void searchBooks(BookData arr[], int dataSize) {
+    long long int searchCategory;
+    long long int searchCode;
+    long long int searchYear;
+    long long int searchMonth;
+    long long int searchDay;
 
-    for (int i = 1; i < len; i++) {
-        int j;
-        for (j = 0; j < currentIndex; j++) {
-            if (input[i] == input[j]) {
+    printf("검색하려는 항목을 선택하세요:\n");
+    printf("1. 도서분류\n");
+    printf("2. 분류기호\n");
+    printf("3. 년도\n");
+    printf("4. 월\n");
+    printf("5. 일\n");
+
+    char input[20];
+
+    // 사용자 입력 받기
+    while (true) {
+        printf("숫자를 입력하세요: ");
+        scanf("%s", input);
+
+        // 입력된 각 숫자를 확인하여 유효성 검사
+        bool validInput = true;
+        for (int i = 0; i < strlen(input); i++) {
+            int digit = input[i] - '0';
+            if (digit < 1 || digit > 5) {
+                validInput = false;
                 break;
             }
         }
-        if (j == currentIndex) {
-            input[currentIndex] = input[i];
-            currentIndex++;
+
+        if (validInput) {
+            // 올바른 값이 입력되면 반복문 종료
+            break;
+        }
+        else {
+            printf("잘못된 입력입니다. 다시 입력하세요.\n");
+            while (getchar() != '\n');  // 버퍼 비우기
         }
     }
 
-    input[currentIndex] = '\0'; // 문자열 끝에 널 문자 추가
+    // 입력된 값 출력 또는 저장할 수 있도록 원하는 동작 수행
+    printf("입력된 값: %s\n", input);
+
+    // 중복된 숫자 처리
+    removeDuplicateDigits(input);
+
+    // 입력에 따라 사용자에게 검색 기준 입력 받음
+    promptUserForSearchCriteria(input, &searchCategory, &searchCode, &searchYear, &searchMonth, &searchDay);
+
+    // 실제 검색 수행
+    bool found = false;
+    int resultIndex = 0;
+
+    printf("[ 검색 결과 ]\n");
+
+    // 검색된 도서코드를 출력
+    for (int i = 0; i < dataSize; i++) {
+        if (
+            (searchCategory == arr[i].category || searchCategory < 0) &&
+            (searchCode == arr[i].code || searchCode < 0) &&
+            (searchYear == arr[i].year || searchYear < 0) &&
+            (searchMonth == arr[i].month || searchMonth < 0) &&
+            (searchDay == arr[i].day || searchDay < 0)
+            ) {
+            resultIndex++;
+            printf("%d. %03lld%03lld%04lld%02lld%02lld\n", resultIndex, arr[i].category, arr[i].code, arr[i].year, arr[i].month, arr[i].day);
+            found = true;
+        }
+    }
+
+    if (!found) {
+        printf("일치하는 도서코드가 없습니다.\n");
+    }
+}
+
+// 도서를 데이터 배열에 추가하는 함수
+void addBook(BookData arr[], int* dataSize, int* currentSize) {
+    if (*dataSize == *currentSize) {
+        // 배열이 꽉 찼을 경우 크기를 두 배로 늘립니다.
+        *currentSize *= 2;
+        arr = realloc(arr, *currentSize * sizeof(BookData));
+        if (arr == NULL) {
+            perror("메모리를 재할당할 수 없습니다.");
+            exit(1);
+        }
+    }
+
+    // 새로운 도서에 대한 입력을 받습니다.
+    printf("도서분류 입력: ");
+    scanf("%lld", &arr[*dataSize].category);
+    printf("분류기호 입력: ");
+    scanf("%lld", &arr[*dataSize].code);
+    printf("년도 입력: ");
+    scanf("%lld", &arr[*dataSize].year);
+    printf("월 입력: ");
+    scanf("%lld", &arr[*dataSize].month);
+    printf("일 입력: ");
+    scanf("%lld", &arr[*dataSize].day);
+
+    (*dataSize)++;
+}
+
+// 도서를 데이터 배열에서 수정하는 함수
+void modifyBook(BookData arr[], int dataSize) {
+    long long int searchCode;
+    printf("수정하려는 도서의 도서코드를 입력하세요: ");
+    scanf("%lld", &searchCode);
+
+    int foundIndex = -1;
+    for (int i = 0; i < dataSize; i++) {
+        if (arr[i].code == searchCode) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    if (foundIndex != -1) {
+        // 수정 사항을 입력받습니다.
+        printf("도서분류 수정: ");
+        scanf("%lld", &arr[foundIndex].category);
+        printf("분류기호 수정: ");
+        scanf("%lld", &arr[foundIndex].code);
+        printf("년도 수정: ");
+        scanf("%lld", &arr[foundIndex].year);
+        printf("월 수정: ");
+        scanf("%lld", &arr[foundIndex].month);
+        printf("일 수정: ");
+        scanf("%lld", &arr[foundIndex].day);
+
+        printf("도서 정보가 수정되었습니다.\n");
+    }
+    else {
+        printf("일치하는 도서코드가 없습니다.\n");
+    }
+}
+
+// 도서를 데이터 배열에서 삭제하는 함수
+void deleteBook(BookData arr[], int* dataSize) {
+    long long int searchCode;
+    printf("삭제하려는 도서의 도서코드를 입력하세요: ");
+    scanf("%lld", &searchCode);
+
+    int foundIndex = -1;
+    for (int i = 0; i < *dataSize; i++) {
+        if (arr[i].code == searchCode) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    if (foundIndex != -1) {
+        // 마지막 도서를 삭제된 위치로 이동하고 dataSize를 감소시킵니다.
+        arr[foundIndex] = arr[*dataSize - 1];
+        (*dataSize)--;
+        printf("도서가 삭제되었습니다.\n");
+    }
+    else {
+        printf("일치하는 도서코드가 없습니다.\n");
+    }
+}
+
+// 메인 메뉴 함수
+void showMainMenu() {
+    printf("도서 관리 프로그램\n");
+    printf("1. 도서코드 검색\n");
+    printf("2. 도서코드 추가\n");
+    printf("3. 도서코드 수정\n");
+    printf("4. 도서코드 삭제\n");
+    printf("0. 프로그램 종료\n");
 }
 
 int main() {
@@ -226,59 +369,39 @@ int main() {
     // 퀵 정렬 수행
     quickSort(data, 0, dataSize - 1);
 
-    // 검색 기능
-    long long int searchCategory = -1;  // -1은 해당 항목을 무시하고 검색
-    long long int searchCode = -1;
-    long long int searchYear = -1;
-    long long int searchMonth = -1;
-    long long int searchDay = -1;
+    int choice = -1;
 
-    // 검색할 항목 선택
-    printf("검색하려는 항목을 선택하세요:\n");
-    printf("1. 도서분류\n");
-    printf("2. 분류기호\n");
-    printf("3. 년도\n");
-    printf("4. 월\n");
-    printf("5. 일\n");
+    do {
+        showMainMenu();
+        printf("원하는 작업을 선택하세요: ");
+        scanf("%d", &choice);
 
-    char input[20];
-    int selectedOption;
-
-    while (true) {
-        printf("숫자를 입력하세요: ");
-        scanf("%s", input);
-
-        // 입력된 각 숫자를 확인하여 유효성 검사
-        bool validInput = true;
-        for (int i = 0; i < strlen(input); i++) {
-            int digit = input[i] - '0';
-            if (digit < 1 || digit > 5) {
-                validInput = false;
-                break;
-            }
-        }
-
-        if (validInput) {
-            // 올바른 값이 입력되면 반복문 종료
+        switch (choice) {
+        case 1:
+            // 도서코드 검색
+            searchBooks(data, dataSize);
+            break;
+        case 2:
+            // 도서코드 추가
+            addBook(data, &dataSize, &currentSize);
+            break;
+        case 3:
+            // 도서코드 수정
+            modifyBook(data, dataSize);
+            break;
+        case 4:
+            // 도서코드 삭제
+            deleteBook(data, &dataSize);
+            break;
+        case 0:
+            // 프로그램 종료
+            printf("프로그램을 종료합니다.\n");
+            break;
+        default:
+            printf("잘못된 입력입니다. 다시 입력하세요.\n");
             break;
         }
-        else {
-            printf("잘못된 입력입니다. 다시 입력하세요.\n");
-            while (getchar() != '\n');  // 버퍼 비우기
-        }
-    }
-
-    // 입력된 값 출력 또는 저장할 수 있도록 원하는 동작 수행
-    printf("입력된 값: %s\n", input);
-
-    // 중복된 숫자 처리
-    removeDuplicateDigits(input);
-
-    // 입력에 따라 사용자에게 검색 기준 입력 받음
-    promptUserForSearchCriteria(input, &searchCategory, &searchCode, &searchYear, &searchMonth, &searchDay);
-
-    // 검색 함수 호출
-    searchBooks(data, dataSize, searchCategory, searchCode, searchYear, searchMonth, searchDay);
+    } while (choice != 0); // 0을 선택하면 루프를 탈출합니다.
 
     // 할당된 메모리 해제
     free(data);
